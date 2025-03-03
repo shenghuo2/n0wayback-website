276d0426bcb36ed015510023c85e884f933f8d2a
shenghuo2 <1308357113@qq.com>
2025-03-03 21:34:35 +0800

    feat: second color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        primary: "#0a192f",
-        secondary: "#64ffda",
-        dark: "#020c1b",
-        light: "#ccd6f6",
+        primary: {
+          dark: "#0a192f",
+          light: "#f5f8ff"
+        },
+        secondary: {
+          dark: "#64ffda", 
+          light: "#009e7f"  
+        },
+        dark: {
+          dark: "#020c1b",
+          light: "#ffffff"
+        },
+        light: {
+          dark: "#ccd6f6",
+          light: "#1a1a1a"
+        },
         highlight: "#ff5555",
       },
       fontFamily: {
